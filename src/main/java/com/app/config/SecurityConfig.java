bb6f953476e91f87877a4390ffb449d226561daa
sajal tiwari <sajaltiwari437@gmail.com>
2025-08-04 20:43:58 +0530

    user

--- src/main/java/com/app/config/SecurityConfig.java
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://laxmishop.netlify.app"));
+        configuration
+                .setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://https://laxmishop.netlify.app/"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
